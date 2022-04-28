621e3284ad5c5e87ab9e10512405dbe3ae1fedf8
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-04-28 13:05:03 -0300

    feat: started project, added files, folders and hooks

---
+++ src/database.js
@@ -0,0 +1,11 @@
+export const dataBase = [
+    { name: 'Monello Dog Premium Especial - Tradicional - 25kgs', price: 245, url: 'https://cdn.awsli.com.br/800x800/701/701406/produto/28659528/c508a1c9c5.jpg', type: 'racao' },
+    { name: 'Birbo Premium Cães Adultos - Tradicional - 7kgs', price: 59, url: 'https://cdn.awsli.com.br/701/701406/produto/28658515/a1a31613a1.jpg', type: 'racao' },
+    { name: 'Brinquedo Para Cães Osso Com Relevo Vinil', price: 14, url: 'https://images.tcdn.com.br/img/img_prod/763413/180_brinquedo_para_caes_osso_com_relevo_vinil_gr211062_8409_1_20200724165036.png', type: 'brinquedo' },
+    { name: 'Brinquedo Pet Bola Cachorro', price: 8, url: 'https://www.petshop25.com.br/image/cache/data/eftr/Img_ftr_rp_2302-1160x1160.PNG?version=20210921195323', type: 'brinquedo' },
+    { name: 'Ivercanis 6mg - Ivermectina P/ Cães - Tratamento Contra Sarnas', price: 23.40, url: 'https://claudioagrorural.loja2.com.br/img/18eae138a91664977e2e13710fc70c27.png', type: 'remedio' },
+    { name: 'Comfortis 560mg 1 Comprimido', price: 129.99, url: 'https://farmaciaindiana.vteximg.com.br/arquivos/ids/226245/7891217000241.png?v=637329321667900000', type: 'remedio' },
+    { name: 'Banho E Tosa Para Cães e Gatos - Vacaria - RS', price: 75, url: 'https://pantanalpet.com.br/wp-content/uploads/2021/01/banhoetosa.png', type: 'servico' },
+    { name: 'Castração Para Cães E Gatos - Vacaria - RS', price: 250, url: 'https://site.ucdb.br//public/textos/1036964-projeto-castracao-novo-feed.png', type: 'servico' },
+    { name: 'Ração GranPlus Carne e Arroz para Gatos Adultos', price: 135, url: 'https://www.petlove.com.br/images/products/208113/product/Ra%C3%A7%C3%A3o_Affinity_PetCare_Gran_Plus_Carne_e_Arroz_para_Gatos_Adultos_1746329_10_1.jpg?1627667127', type: 'racao' }
+]
---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+export default function Routes() {
+	return (
+		<Switch>
+			<Route exact path="/"></Route>
+			<Route exact path="/"></Route>
+			<Route exact path="/"></Route>
+			<Route exact path="/"></Route>
+		</Switch>
+	);
+}
